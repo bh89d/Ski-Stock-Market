bd2f78fdf49fe99215229d73ed4ca578fa672529
bh89d <bhaumiktrivedi08@gmail.com>
2026-07-01 17:32:43 +0530

    checks done

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-console.log("ENV:", import.meta.env);
-console.log("API:", import.meta.env.VITE_API_URL);
-
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
